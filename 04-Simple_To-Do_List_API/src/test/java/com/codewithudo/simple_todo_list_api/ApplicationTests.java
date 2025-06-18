717e0e0f12a0b4aaecc08f93dd73913570a58420
Onuegbu Udochukwu <onuegbuudochukwu6@gmail.com>
2025-06-18 12:26:25 +0100

    Todo List File Structure

---
+++ 04-Simple_To-Do_List_API/src/test/java/com/codewithudo/simple_todo_list_api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithudo.simple_todo_list_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
