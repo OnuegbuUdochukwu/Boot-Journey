4c52cf9099efc2f332f99949a49bf2d7ff86f1eb
Onuegbu Udochukwu <onuegbuudochukwu6@gmail.com>
2025-06-19 18:16:55 +0100

    Contact Service

--- 05-contact-book-api/src/main/java/com/codewithudo/contactbookapi/service/ContactService.java
+++ 05-contact-book-api/src/main/java/com/codewithudo/contactbookapi/service/ContactService.java
@@ -1,4 +1,42 @@
 package com.codewithudo.contactbookapi.service;
 
+import com.codewithudo.contactbookapi.model.Contact;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+
+@Service
 public class ContactService {
+
+    ArrayList<Contact> contacts = new ArrayList<>();
+
+    public ArrayList<Contact> getContacts() {
+        return contacts;
+    }
+    public Contact getContactById(int id) {
+        for (Contact contact : contacts) {
+            if (contact.getId() == id)
+                return contact;
+        }
+        return null;
+    }
+
+    public ArrayList<Contact> addContact(Contact contact) {
+        contacts.add(contact);
+        return contacts;
+    }
+
+    public ArrayList<Contact> removeContact(Contact contact) {
+        contacts.remove(contact);
+        return contacts;
+    }
+
+    public ArrayList<Contact> updateContact(int id, Contact contact) {
+        contacts.get(id).setName(contact.getName());
+        contacts.get(id).setAddress(contact.getAddress());
+        contacts.get(id).setEmail(contact.getEmail());
+        contacts.get(id).setPhone(contact.getPhone());
+        return contacts;
+
+    }
+
 }
