afdf941654cb637f8c8501b9ee1bec017eabf88c
Onuegbu Udochukwu <onuegbuudochukwu6@gmail.com>
2025-06-19 18:01:37 +0100

    Contact Model

--- 05-contact-book-api/src/main/java/com/codewithudo/contactbookapi/model/Contact.java
+++ 05-contact-book-api/src/main/java/com/codewithudo/contactbookapi/model/Contact.java
@@ -1,4 +1,19 @@
 package com.codewithudo.contactbookapi.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 public class Contact {
+    private int id;
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
 }
