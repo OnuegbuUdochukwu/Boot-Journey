9c5f95be8d7d8ae170280a4c02937b2bc8e3ea27
Onuegbu Udochukwu <onuegbuudochukwu6@gmail.com>
2025-06-24 01:58:28 +0100

    Model

--- 07-student-management-system/src/main/java/com/codewithudo/studentmanagementsystem/model/Student.java
+++ 07-student-management-system/src/main/java/com/codewithudo/studentmanagementsystem/model/Student.java
@@ -1,4 +1,16 @@
 package com.codewithudo.studentmanagementsystem.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
+    private Integer id;
+    private String name;
+    private String email;
+    private String course;
+    private int age;
 }
