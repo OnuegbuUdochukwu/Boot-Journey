f9d756e5b5a83d1b767499da2dd7ba3931260b8f
Onuegbu Udochukwu <onuegbuudochukwu6@gmail.com>
2025-06-27 14:07:25 +0100

    Integrating OpenWeather api to getWeatherData

--- 06-weather-information-api/src/main/java/com/codewithudo/weatherinformationapi/service/WeatherService.java
+++ 06-weather-information-api/src/main/java/com/codewithudo/weatherinformationapi/service/WeatherService.java
@@ -1,30 +1,57 @@
 package com.codewithudo.weatherinformationapi.service;
 
+import com.codewithudo.weatherinformationapi.model.OpenWeatherResponse;
 import com.codewithudo.weatherinformationapi.model.WeatherData;
+//import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 
 @Service
 public class WeatherService {
 
-    private final ArrayList<WeatherData> weatherDataList = new ArrayList<>();
+    @Value("${openweathermap.api.key}")
+    private String apiKey;
 
-    // Constructor: Populate static data when service starts
-    public WeatherService() {
-        weatherDataList.add(new WeatherData("Lagos", 31.5, "Sunny"));
-        weatherDataList.add(new WeatherData("Abuja", 29.0, "Cloudy"));
-        weatherDataList.add(new WeatherData("London", 17.8, "Rainy"));
-        weatherDataList.add(new WeatherData("New York", 23.0, "Partly Cloudy"));
-    }
-    public ArrayList<WeatherData> getWeatherDataList() {
-        return weatherDataList;
+    @Value("${openweathermap.api.url}")
+    private String apiUrl;
 
-    }
+    private final RestTemplate restTemplate = new RestTemplate();
+
+//    private final ArrayList<WeatherData> weatherDataList = new ArrayList<>();
+
+//    // Constructor: Populate static data when service starts
+//    public WeatherService() {
+//        weatherDataList.add(new WeatherData("Lagos", 31.5, "Sunny"));
+//        weatherDataList.add(new WeatherData("Abuja", 29.0, "Cloudy"));
+//        weatherDataList.add(new WeatherData("London", 17.8, "Rainy"));
+//        weatherDataList.add(new WeatherData("New York", 23.0, "Partly Cloudy"));
+//    }
+//    public ArrayList<WeatherData> getWeatherDataList() {
+//        return weatherDataList;
+//    }
+
+
+
     public WeatherData getWeatherData(String city) {
-        return weatherDataList.stream()
-                .filter(data -> data.getCity().equalsIgnoreCase(city))
-                .findFirst()
-                .orElse(null);
+//        String url = String.format("%s?q=%s&units=metric&appid=%s", apiUrl, city, apiKey);
+        String url = apiUrl + "?q=" + city + "&units=metric&appid=" + apiKey;
+//        System.out.println("API URL: " + apiUrl);
+//        System.out.println("API Key: " + apiKey);
+
+        try {
+            OpenWeatherResponse response = restTemplate.getForObject(url, OpenWeatherResponse.class);
+            if (response != null) {
+                return new WeatherData(response.getCity(), response.getTemp(), response.getDescription());
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            System.err.println("Error fetching data: " + e.getMessage());
+            return null;
+        }
+
     }
 }
