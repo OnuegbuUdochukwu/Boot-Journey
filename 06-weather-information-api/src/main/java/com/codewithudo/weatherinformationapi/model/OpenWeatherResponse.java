a4a9a0ccc2c2e3e16c2999a41a7d2b22c40a6035
Onuegbu Udochukwu <onuegbuudochukwu6@gmail.com>
2025-06-27 13:43:25 +0100

    Adding API key

---
+++ 06-weather-information-api/src/main/java/com/codewithudo/weatherinformationapi/model/OpenWeatherResponse.java
@@ -0,0 +1,8 @@
+package com.codewithudo.weatherinformationapi.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class OpenWeatherResponse {
+
+}
