f9eb9e8645b820c8f3994743aa1033353f6e30ae
Onuegbu Udochukwu <onuegbuudochukwu6@gmail.com>
2025-06-29 20:15:36 +0100

    Note Service with Functions

--- 09-basic-notes-app-api/src/main/java/com/codewithudo/basicnotesappapi/service/NoteService.java
+++ 09-basic-notes-app-api/src/main/java/com/codewithudo/basicnotesappapi/service/NoteService.java
@@ -1,4 +1,47 @@
 package com.codewithudo.basicnotesappapi.service;
 
+import com.codewithudo.basicnotesappapi.model.Note;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
 public class NoteService {
+
+    private ArrayList<Note> notes = new ArrayList<>();
+    private int numberOfNotes = 1;
+
+//    public NoteService(ArrayList<Note> notes) {
+//        this.notes = notes;
+//        this.numberOfNotes = notes.size();
+//    }
+
+    public ArrayList<Note> getAllNotes(){
+        return notes;
+    }
+
+    public Note getNoteById(int id){
+        return notes.get(id);
+    }
+
+    public boolean addNote(Note note) {
+        numberOfNotes++;
+        return notes.add(note);
+    }
+
+    public boolean updateNote(int id, Note note) {
+        for(Note n : notes) {
+            if(n.getId() == id){
+                note.setTitle(note.getTitle());
+                note.setContent(note.getContent());
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean removeNote(Note note) {
+        numberOfNotes--;
+        return notes.remove(note);
+    }
 }
