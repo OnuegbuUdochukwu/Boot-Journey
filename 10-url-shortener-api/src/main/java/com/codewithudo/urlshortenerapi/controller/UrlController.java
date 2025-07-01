0aa4d7633db25be1c98589e6d2a0d08297234cad
Onuegbu Udochukwu <onuegbuudochukwu6@gmail.com>
2025-07-02 00:17:22 +0100

    Controller endpoints added

--- 10-url-shortener-api/src/main/java/com/codewithudo/urlshortenerapi/controller/UrlController.java
+++ 10-url-shortener-api/src/main/java/com/codewithudo/urlshortenerapi/controller/UrlController.java
@@ -1,4 +1,38 @@
 package com.codewithudo.urlshortenerapi.controller;
 
+import com.codewithudo.urlshortenerapi.model.UrlMapping;
+import com.codewithudo.urlshortenerapi.service.UrlService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/api/url")
 public class UrlController {
+
+    @Autowired
+    private UrlService urlService;
+
+    @PostMapping
+    public ResponseEntity<UrlMapping> addUrlMapping(@RequestBody String originalUrl) {
+        UrlMapping urlMapping = urlService.addUrlMapping(originalUrl);
+        return new ResponseEntity<>(urlMapping, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{shortCode}")
+    public ResponseEntity<String> getUrlMapping(@PathVariable String shortCode) {
+        String originalUrl = urlService.getOriginalUrl(shortCode);
+        if (originalUrl.equals("Not Found")) {
+            return new ResponseEntity<>("URL not found", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(originalUrl, HttpStatus.FOUND);
+    }
+
+    @GetMapping
+    public ResponseEntity<ArrayList<UrlMapping>> getAllMappings() {
+        return new ResponseEntity<>(urlService.getAllUrlMappings(), HttpStatus.OK);
+    }
 }
