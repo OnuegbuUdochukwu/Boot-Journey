ce08d098083be863ec77688bcfea1ec3ca157128
Onuegbu Udochukwu <onuegbuudochukwu6@gmail.com>
2025-07-01 14:31:33 +0100

    Url Mapping complete

--- 10-url-shortener-api/src/main/java/com/codewithudo/urlshortenerapi/model/UrlMapping.java
+++ 10-url-shortener-api/src/main/java/com/codewithudo/urlshortenerapi/model/UrlMapping.java
@@ -1,4 +1,14 @@
 package com.codewithudo.urlshortenerapi.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UrlMapping {
+    private int id;
+    private String originalUrl;
+    private String shortCode;
 }
