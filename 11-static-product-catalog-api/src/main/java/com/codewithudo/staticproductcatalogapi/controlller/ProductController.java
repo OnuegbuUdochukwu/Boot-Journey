30e37fd1e0e298b45c8786921c6f4596423215ac
Onuegbu Udochukwu <onuegbuudochukwu6@gmail.com>
2025-07-02 10:32:34 +0100

    Controller endpoints

--- 11-static-product-catalog-api/src/main/java/com/codewithudo/staticproductcatalogapi/controlller/ProductController.java
+++ 11-static-product-catalog-api/src/main/java/com/codewithudo/staticproductcatalogapi/controlller/ProductController.java
@@ -1,9 +1,37 @@
 package com.codewithudo.staticproductcatalogapi.controlller;
 
+import com.codewithudo.staticproductcatalogapi.model.Product;
+import com.codewithudo.staticproductcatalogapi.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/api/product")
 public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<ArrayList<Product>> getAllProducts() {
+        ArrayList<Product> products = productService.getAllProducts();
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable int id) {
+        Product product = productService.getProductById(id);
+        if (product != null) {
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
 }
