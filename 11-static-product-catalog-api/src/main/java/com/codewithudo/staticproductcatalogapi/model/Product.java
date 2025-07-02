9f6adb75ec545ee0dbafd765d473c6f21f648021
Onuegbu Udochukwu <onuegbuudochukwu6@gmail.com>
2025-07-02 10:17:04 +0100

    Data Filled

--- 11-static-product-catalog-api/src/main/java/com/codewithudo/staticproductcatalogapi/model/Product.java
+++ 11-static-product-catalog-api/src/main/java/com/codewithudo/staticproductcatalogapi/model/Product.java
@@ -1,7 +1,16 @@
 package com.codewithudo.staticproductcatalogapi.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
+    private Integer id;
+    private String name;
+    private String description;
+    private Double price;
+    private Boolean available;
 }
