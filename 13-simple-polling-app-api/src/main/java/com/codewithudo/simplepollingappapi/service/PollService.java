baf4c3b52e3cad96948cb22caf90ac90516c1d45
Onuegbu Udochukwu <onuegbuudochukwu6@gmail.com>
2025-07-03 18:17:05 +0100

    Poll Service class

---
+++ 13-simple-polling-app-api/src/main/java/com/codewithudo/simplepollingappapi/service/PollService.java
@@ -0,0 +1,11 @@
+package com.codewithudo.simplepollingappapi.service;
+
+import com.codewithudo.simplepollingappapi.model.Poll;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PollService {
+}
