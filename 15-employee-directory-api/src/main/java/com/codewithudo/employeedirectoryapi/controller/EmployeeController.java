73a6defbe4afc46be8d7896e4897c8d2ad96e473
Onuegbu Udochukwu <onuegbuudochukwu6@gmail.com>
2025-07-08 09:07:32 +0100

    Employee COntroller

---
+++ 15-employee-directory-api/src/main/java/com/codewithudo/employeedirectoryapi/controller/EmployeeController.java
@@ -0,0 +1,64 @@
+package com.codewithudo.employeedirectoryapi.controller;
+
+import com.codewithudo.employeedirectoryapi.model.Employee;
+import com.codewithudo.employeedirectoryapi.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employee")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    // Create a new employee
+    @PostMapping
+    public ResponseEntity<String> addEmployee(@RequestBody Employee employee) {
+        boolean added = employeeService.addEmployee(employee);
+        if (added) {
+            return new ResponseEntity<>("Employee added successfully", HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>("Failed to add employee", HttpStatus.BAD_REQUEST);
+    }
+
+    // Get all employees
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        return new ResponseEntity<>(employeeService.getAllEmployees(), HttpStatus.OK);
+    }
+
+    // Get an employee by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployee(@PathVariable Integer id) {
+        Employee employee = employeeService.getEmployeeById(id);
+        if (employee == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
+
+    // Update an employee by ID
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateEmployee(@PathVariable int id, @RequestBody Employee employee) {
+        boolean updated = employeeService.updateEmployee(id, employee);
+        if (updated) {
+            return new ResponseEntity<>("Employee updated successfully", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Employee not found", HttpStatus.NOT_FOUND);
+    }
+
+    // Delete an employee by ID
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable int id) {
+        boolean deleted = employeeService.deleteEmployee(id);
+        if (deleted) {
+            return new ResponseEntity<>("Employee deleted successfully", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Employee not found", HttpStatus.NOT_FOUND);
+    }
+}
