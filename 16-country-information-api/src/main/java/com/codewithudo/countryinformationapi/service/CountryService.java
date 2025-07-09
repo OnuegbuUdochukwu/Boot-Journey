82c15da88bdde6d4730022a16c29186bc85fa528
Onuegbu Udochukwu <onuegbuudochukwu6@gmail.com>
2025-07-09 14:30:51 +0100

    Country Service

---
+++ 16-country-information-api/src/main/java/com/codewithudo/countryinformationapi/service/CountryService.java
@@ -0,0 +1,66 @@
+package com.codewithudo.countryinformationapi.service;
+
+import com.codewithudo.countryinformationapi.model.Country;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CountryService {
+
+    private final List<Country> countries = new ArrayList<>();
+    private int nextId = 1;
+
+    // Add a new country
+    public boolean addCountry(Country country) {
+        country.setId(nextId++);
+        countries.add(country);
+        return true;
+    }
+
+    // Get all countries
+    public List<Country> getAllCountries() {
+        return countries;
+    }
+
+    // Get a country by ID
+    public Country getCountryById(int id) {
+        for (Country country : countries) {
+            if (country.getId().equals(id)) {
+                return country;
+            }
+        }
+        return null;
+    }
+
+    // Get a country by Name
+    public Country getCountryByName(String name) {
+        for (Country country : countries) {
+            if (country.getName().equals(name)) {
+                return country;
+            }
+        }
+        return null;
+    }
+
+    // Update a country by ID
+    public boolean updateCountry(int id, Country updatedCountry) {
+        for (Country country : countries) {
+            if (country.getId().equals(id)) {
+                country.setName(updatedCountry.getName());
+                country.setCapital(updatedCountry.getCapital());
+                country.setRegion(updatedCountry.getRegion());
+                country.setPopulation(updatedCountry.getPopulation());
+                country.setCurrency(updatedCountry.getCurrency());
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Delete a country by ID
+    public boolean deleteCountry(int id) {
+        return countries.removeIf(country -> country.getId().equals(id));
+    }
+}
