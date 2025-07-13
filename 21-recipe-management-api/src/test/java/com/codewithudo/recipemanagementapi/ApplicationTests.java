79448b09d21828ac98d42303aadd641b6f8a144c
Onuegbu Udochukwu <onuegbuudochukwu6@gmail.com>
2025-07-13 23:26:27 +0100

    21 Folder Structure

---
+++ 21-recipe-management-api/src/test/java/com/codewithudo/recipemanagementapi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithudo.recipemanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
