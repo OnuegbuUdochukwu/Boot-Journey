32bc2525b86170415a0138a6df6e2fd18d31ad48
Onuegbu Udochukwu <onuegbuudochukwu6@gmail.com>
2025-07-15 21:28:23 +0100

    Currency Data (ConversionRequest) Data

---
+++ 25-currency-converter-api/src/main/java/com/codewithudo/currencyconverterapi/model/ConversionRequest.java
@@ -0,0 +1,14 @@
+package com.codewithudo.currencyconverterapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ConversionRequest {
+    private String fromCurrency;
+    private String toCurrency;
+    private double amount;
+}
