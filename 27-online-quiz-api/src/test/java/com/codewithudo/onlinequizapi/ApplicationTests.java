3cacec7f94c8006de21017ba5df3352266c54893
Onuegbu Udochukwu <onuegbuudochukwu6@gmail.com>
2025-07-18 02:27:12 +0100

    27 Folder Structure

---
+++ 27-online-quiz-api/src/test/java/com/codewithudo/onlinequizapi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithudo.onlinequizapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
