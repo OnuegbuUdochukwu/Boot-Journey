0adf33fc5085dcfc9ead74146e9a1e0023b6eda0
Onuegbu Udochukwu <onuegbuudochukwu6@gmail.com>
2025-07-29 21:48:06 +0100

    Added Entity Annotation

--- 30-e-library-book-lending/src/main/java/com/codewithudo/elibrarybooklending/model/Book.java
+++ 30-e-library-book-lending/src/main/java/com/codewithudo/elibrarybooklending/model/Book.java
@@ -1,12 +1,16 @@
 package com.codewithudo.elibrarybooklending.model;
 
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 
+@Entity
 @Data
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
--- 30-e-library-book-lending/src/main/java/com/codewithudo/elibrarybooklending/model/BorrowRecord.java
+++ 30-e-library-book-lending/src/main/java/com/codewithudo/elibrarybooklending/model/BorrowRecord.java
@@ -1,15 +1,22 @@
 package com.codewithudo.elibrarybooklending.model;
 
+
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
+@Entity
 @Data
 public class BorrowRecord {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne
     private Book book;
+
 
     private String borrowerName;
 
